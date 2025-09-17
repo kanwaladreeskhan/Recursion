0cb478837179264a4faf4888981abb9f1b3bfbdc
kanwaladreeskhan <khanadreiskhan@gmail.com>
2025-09-17 12:27:34 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSequence(5,"*");
+        }
+        public static void PrintSequence(int n,String symbol)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine(symbol);
+                PrintSequence(n-1, symbol);
+            }
+        }
+    }
+}
